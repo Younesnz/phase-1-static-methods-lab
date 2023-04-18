f6fb24037b20a4dd4152af4d97a8425afb9e6ad3
Younes Nourzehi <unesnz@gmail.com>
2023-04-18 14:44:22 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
 class Formatter {
-  //add static methods here
+  static capitalize(text) {
+    return text[0].toUpperCase() + text.slice(1);
+  }
+  static sanitize(text) {
+    let output = "";
+    for (let char of text) {
+      if (
+        /^[A-Za-z0-9]*$/.test(char) ||
+        char === "'" ||
+        char === "-" ||
+        char === " "
+      ) {
+        output += char;
+      }
+    }
+    return output;
+  }
+  static titleize(text) {
+    text = text.split(" ");
+    text = text.map((word, index) => {
+      if (
+        index === 0 ||
+        (word !== "the" &&
+          word !== "a" &&
+          word !== "an" &&
+          word !== "but" &&
+          word !== "of" &&
+          word !== "and" &&
+          word !== "for" &&
+          word !== "at" &&
+          word !== "by" &&
+          word !== "from" &&
+          word)
+      )
+        word = this.capitalize(word);
+      return word;
+    });
+    return text.join(" ");
+  }
 }
